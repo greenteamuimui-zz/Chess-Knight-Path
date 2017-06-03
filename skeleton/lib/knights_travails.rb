0472f4e9b0ee04d6b559fb4e0f67ce54324c80b1
App Academy Student <student@appacademy.io>
2017-06-02 17:01:12 -0700

    Created valid_moves class method and new move postitions method in knights path finder

--- skeleton/lib/knights_travails.rb
+++ skeleton/lib/knights_travails.rb
@@ -1,6 +1,8 @@
+require_relative "00_tree_node"
+
 class KnightPathFinder
   MOVES = [[-1,2], [1,2], [2,1], [2,-1], [1,-2], [-1,-2], [-2,-1], [-2,1]]
-
+  attr_reader :starting_pos, :visited_positions, :move_tree
 
    def initialize(arr_pos)
      @starting_pos = arr_pos
@@ -9,7 +11,13 @@ class KnightPathFinder
    end
 
    def build_move_tree
-
+    #  queue = [self]
+    #  until queue.empty?
+    #    current_node = queue.shift
+    #    return current_node if current_node.value == target_value
+    #    queue.concat(current_node.children)
+    #  end
+    #  nil
    end
 
    def find_path(final_destination)
@@ -17,11 +25,23 @@ class KnightPathFinder
    end
 
    def self.valid_moves(pos)
-
+     all_moves = []
+     x, y = pos
+     MOVES.each_with_index do |array, i|
+       x_check = x + array[0]
+       y_check = y + array[1]
+       if x_check.between?(0,7) && y_check.between?(0,7)
+         all_moves << array
+       end
+     end
+     all_moves
    end
 
    def new_move_positions(pos)
-
+     if KnightPathFinder.valid_moves(pos) && !@visited_positions.include?(pos)
+       @visited_positions << pos
+       return pos
+     end
    end
 
 end
